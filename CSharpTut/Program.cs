07d09ce35146b75c139b91d7760dd787da0e51db
Sean Drohan <sean.drohan@gmail.com>
2019-02-15 16:32:01 -0500

    More learning.

--- CSharpTut/Program.cs
+++ CSharpTut/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace CSharpTut
 {
@@ -10,34 +11,31 @@ namespace CSharpTut
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.WriteLine($"This is the current time {DateTime.Now}");
 
-            for (int i=0; i < args.Length; i++)
+            //Console.Write("Enter your age: ");
+            //int drivingAge = int.Parse(Console.ReadLine());
+
+            Random rnd = new Random();
+
+            int secretNumber = rnd.Next(1, 11);
+            int numberGuessed = 0;
+
+            do
             {
-                Console.WriteLine($"Arg {i} is: {args[i]}");
+                Console.Write("Guss a number between 1 and 10:");
+                numberGuessed = int.Parse(Console.ReadLine());
+
             }
+            while (secretNumber != numberGuessed);
 
+            Console.Write($"You guessed the secret number: {secretNumber}");
 
-            string[] myArgs = Environment.GetCommandLineArgs();
+            Console.ReadLine();
 
-            Console.WriteLine(string.Join(", ", myArgs));
-
-            SayHello();
-
-            Console.ReadKey();
-
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
 
-        private static void SayHello()
-        {
-            string name = "";
-            Console.Write("What is your name: ");
-            name = Console.ReadLine();
-            Console.WriteLine("Hello " + name);
-        }
+
+
+
     }
 }
